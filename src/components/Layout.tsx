bfe12c96178d9100b9e17c2a8f07fa5485f15720
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-04-15 10:23:46 +0200

    Navigation bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,39 +1,81 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { Button } from "./ui/Button";
-import { CalendarClock, Home as HomeIcon, LineChart, Smile } from "lucide-react";
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import {Outlet} from "react-router-dom";
 
 export default function Layout() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        (ref.current as HTMLDivElement).ownerDocument.body.scrollTop = 0;
+    }, [value]);
+
     return (
-        <div>
-            <Outlet />
-            <nav>
-                <ul className="flex list-none p-0">
-                    <li className="mr-4">
-                        <Button asChild>
-                            <Link to="/">
-                                <HomeIcon />
-                            </Link>
-                        </Button>
-                    </li>
-                    <li className="mr-4">
-                        <Link to="/infos">
-                            <LineChart />
-                        </Link>
-                    </li>
-                    <li className="mr-4">
-                        <Link to="/calendar">
-                            <CalendarClock />
-                        </Link>
-                    </li>
-                    <li className="mr-4">
-                        <Link to="/profile">
-                            <Smile />
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <hr />
-        </div>
+        <Box sx={{pb: 7}} ref={ref}>
+            <CssBaseline/>
+            <Outlet/>
+            <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="Recents" icon={<AccountCircleIcon/>}/>
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon/>}/>
+                    <BottomNavigationAction label="Archive" icon={<ArchiveIcon/>}/>
+                </BottomNavigation>
+            </Paper>
+        </Box>
     );
 }
+//     {/*<Outlet/>*/
+//     }
+//     {/*<BottomNavigation*/
+//     }
+//     {/*    showLabels*/
+//     }
+//     {/*    value={value}*/
+//     }
+//     {/*    onChange={(event, newValue) => {*/
+//     }
+//     {/*        setValue(newValue);*/
+//     }
+//     {/*    }}*/
+//     }
+//     {/*>*/
+//     }
+//     {/*    <BottomNavigationAction label="Home" icon={<HomeIcon/>}>*/
+//     }
+//     {/*        <Link to={'/'}></Link>*/
+//     }
+//     {/*    </BottomNavigationAction>*/
+//     }
+//     {/*    <BottomNavigationAction label="Charts" icon={<LineChart/>}>*/
+//     }
+//     {/*        <Link to={'/charts'}></Link>*/
+//     }
+//     {/*    </BottomNavigationAction>*/
+//     }
+//     {/*    <BottomNavigationAction label="Calendar" icon={<CalendarClock/>}/>*/
+//     }
+//     {/*    <BottomNavigationAction label="Profile" icon={<Smile/>}/>*/
+//     }
+// //     {/*</BottomNavigation>*/
+// //     }
+// // )
+// //     ;
+// // }

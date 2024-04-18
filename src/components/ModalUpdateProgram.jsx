bafbc34cde39f568e3b0cd24774bb335313021eb
Clément Gambier <Clem83Gambier@gmail.com>
2024-04-18 22:02:19 +0200

    chart

--- src/components/ModalUpdateProgram.jsx
+++ src/components/ModalUpdateProgram.jsx
@@ -105,8 +105,8 @@ const ModalUpdateProgram = ({ setIsUpdateModalOpen, date, entity }) => {
             <div className="flex justify-center">
               <button
                 className="text-white inline-flex items-center bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={async () => {
-                  await updateSleepProgram(date, entity, sleepTime, wakeTime);
+                onClick={() => {
+                  updateSleepProgram(date, entity, sleepTime, wakeTime);
                   // setIsUpdateModalOpen(false);
                 }}
               >
--- src/pages/Night.jsx
+++ src/pages/Night.jsx
@@ -1,8 +1,133 @@
-function Night() {
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  LineController,
+  PointElement,
+  LineElement,
+} from "chart.js";
+
+const Night = () => {
+  const [sleepData, setSleepData] = useState([]);
+  const [isSimulating, setIsSimulating] = useState(false);
+
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    LineController,
+    PointElement,
+    LineElement
+  );
+
+  const generateSleepData = async () => {
+    const sleepData = [];
+    let currentTime = 0;
+    while (currentTime < 480) {
+      const duration = Math.floor(Math.random() * 60) + 30; // Durée aléatoire entre 30 et 90 minutes
+      const state = Math.floor(Math.random() * 3);
+      setSleepData((prevSleepData) => [
+        ...prevSleepData,
+        { time: currentTime, duration, state },
+      ]);
+      currentTime += duration;
+    }
+    setIsSimulating(false);
+    return sleepData;
+  };
+
+  useEffect(() => {
+    if (isSimulating) {
+      console.log("Simulation started");
+      generateSleepData();
+    }
+  }, [isSimulating, sleepData]);
+
+  const getStateName = (state) => {
+    switch (state) {
+      case 0:
+        return "REM";
+      case 1:
+        return "Deep";
+      case 2:
+        return "Core";
+      default:
+        return "Awake";
+    }
+  };
+
+  const chartData = {
+    labels: sleepData.map((data) => data.time),
+    datasets: [
+      {
+        label: "Sleep State",
+        data: sleepData.map((data) => ({ y: data.state, x: data.time })), // Utilisez yAxisID pour lier l'état correspondant
+        fill: false,
+        borderColor: "white",
+        tension: 0.1,
+        yAxisID: "states",
+      },
+    ],
+  };
+
+  const chartOptions = {
+    scales: {
+      y: {
+        id: "states",
+        type: "category",
+        labels: [
+          "Sommeil paradoxal",
+          "Sommeil profond",
+          "Sommeil léger",
+          "État classique",
+        ],
+      },
+    },
+  };
+
   return (
     <div>
-      <h1>Night</h1>
+      <h1>Suivi du sommeil</h1>
+      <button
+        type="submit"
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        onClick={() => {
+          setIsSimulating((prevIsSimulating) => !prevIsSimulating);
+        }}
+      >
+        {isSimulating
+          ? "Arrêter le suivi du sommeil.."
+          : "Démarrer le suivi du sommeil"}
+      </button>
+      <div className="p-4 bg-blue-500 rounded-lg shadow-lg">
+        <Line data={chartData} options={chartOptions} />
+      </div>
+      {chartData.labels.length > 0 && (
+        <div>
+          <h2>Détails du sommeil</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Temps</th>
+                <th>Durée</th>
+                <th>État</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sleepData.map((data, index) => (
+                <tr key={index}>
+                  <td>{data.time}</td>
+                  <td>{data.duration}</td>
+                  <td>{getStateName(data.state)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
-}
+};
+
 export default Night;

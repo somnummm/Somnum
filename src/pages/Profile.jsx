dd2b8fc84fce8bcd17751c43488a001cfb21a87c
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-04-29 11:03:03 +0200

    Amélioration du style :art:

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -31,16 +31,14 @@ const Profile = () => {
         </div>
     ) : (
         <>
-            <div className="container">
-                <PageTitle title={`Bienvenue ${user.firstName} ${user.lastName}`}/>
-            </div>
-            <div className="infos">
+            <PageTitle title={`Bienvenue ${user.firstName} ${user.lastName}`}/>
+            <div className="infos justify-start leading-6 pb-4 mx-4">
                 <ul>
-                    <li className="block">{user.age} ans</li>
-                    <li className="block">{user.job}</li>
+                    <li className="block">Age: {user.age} ans</li>
+                    <li className="block">Profession: {user.job}</li>
                 </ul>
             </div>
-            <div className="justify-start leading-6 pb-14 mx-4">
+            <div className="justify-center leading-6 pb-4 mx-4">
                 <ul className="border border-solid border-white rounded-lg pl-2 mr-16 shadow-md">
                     <h1 className="contactTitle">Vos coordonnées : </h1>
                     <li className="mail">{user.email}</li>
@@ -54,11 +52,10 @@ const Profile = () => {
                     <div className="blocInfos">
                         <p className="pageInfosParagraphe">
                             Ce soir, vous devez vous coucher à <p
-                            className="text-red-500 inline">{sleepTime}</p> pour
-                            vous réveiller demain à <span className="text-red-500 inline">{wakeTime}</span>.
+                            className="text-red-400 font-bold inline">{sleepTime}</p> pour
+                            vous réveiller demain à <span className="text-red-400 font-bold inline">{wakeTime}</span>.
                             <br/><br/>
-                            Votre programme de sommeil a défini l'heure à laquelle vous devez
-                            aller vous coucher en vous alertant 30 mins avant.
+                            Vous serez notifié 30 minutes avant d'aller dormir.
                         </p>
                     </div>
                 </div>
@@ -67,10 +64,9 @@ const Profile = () => {
                 className="border border-solid border-gray-300 rounded-lg p-4 mt-4 mx-4 pl-10 bg-white opacity-70 text-black">
                 <Citation/>
             </div>
-            <div className="flex items-center justify-center pt-10">
+            <div className="flex items-center justify-center pt-4">
                 <button
                     type="submit"
-                    // className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                     onClick={async () => {
                         await supabase.auth.signOut();

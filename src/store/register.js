e76274f340316b4d7deda4fbbd13aaed720a826f
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-05-02 15:19:20 +0200

    Register errors management :sparkles:

--- src/store/register.js
+++ src/store/register.js
@@ -6,6 +6,11 @@ const api = () => {
     return mande(`${apiUrl}`);
 };
 
+const errorMessages = {
+    'User already registered': 'Cette adresse e-mail est déjà utilisée.',
+    // Ajoutez d'autres messages d'erreur à traduire ici...
+};
+
 export async function register(email, password, firstName, lastName, age, job) {
     if (!email || !firstName || !lastName || !password || !age || !job) {
         throw new Error("Tous les champs sont obligatoires.");
@@ -16,11 +21,23 @@ export async function register(email, password, firstName, lastName, age, job) {
         throw new Error("L'adresse e-mail n'est pas valide.");
     }
 
-    return await supabase.auth.signUp({
-        email: email,
-        password: password,
-    }).then(async response => {
-        await supabase.auth.updateUser({
+    if (password.length < 6) {
+        throw new Error("Le mot de passe doit contenir au moins 6 caractères.");
+    }
+
+    try {
+        const {user, error} = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            const translatedMessage = errorMessages[error.message] || 'Une erreur s\'est produite lors de l\'inscription.';
+            throw new Error(translatedMessage);
+        }
+
+
+        const updateUser = await supabase.auth.updateUser({
             data: {
                 "firstName": firstName,
                 "lastName": lastName,
@@ -28,5 +45,14 @@ export async function register(email, password, firstName, lastName, age, job) {
                 "job": job,
             },
         });
-    });
+
+        if (updateUser.error) {
+            const translatedMessage = errorMessages[updateUser.error.message] || 'Une erreur s\'est produite lors de la mise à jour de l\'utilisateur.';
+            throw new Error(translatedMessage);
+        }
+
+        return user;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
